b70d0e1ffffba1db6eeea5913abab694432f1d06
jason <jk17@ualberta.ca>
2014-06-08 10:54:52 -0700

    init

---
+++ README.md
@@ -0,0 +1,41 @@
+# FileFog Provider
+
+[![wercker status](https://app.wercker.com/status/ef333f2ec4b0e2f37d93169cf1331423/s "wercker status")](https://app.wercker.com/project/bykey/ef333f2ec4b0e2f37d93169cf1331423)[![Dependency Status](https://gemnasium.com/filefog/filefog-google.png)](https://gemnasium.com/filefog/filefog-google)
+
+A [Filefog](https://github.com/filefog/filefog) adapter for Box.net.
+
+## Install
+
+Install is through NPM.
+
+```bash
+$ npm install filefog-box --save
+```
+
+## Configuration
+
+The following config options are required:
+
+```javascript
+config: {
+    client_key : '',
+    client_secret : '',
+    redirect_url : 'http://localhost:3000/service/callback/box'
+};
+```
+
+## Testing
+
+Test are written with mocha. Integration tests are handled by the [filefog-provider-tests](https://github.com/filefog/filefog-provider-tests) project, which tests provider methods against the latest FileFog API.
+
+To run tests:
+
+```bash
+$ npm test
+```
+
+## About FileFog
+
+FileFog is a cloud agnostic file API.  It provides a uniform API for accessing stuff from different kinds of cloud providers and file systems.  That means you write the same code to manipulate files, whether they live in google drive, dropbox, box.net, or your local file system.
+
+To learn more visit the project on GitHub at [FileFog](https://github.com/filefog/filefog).
---
+++ test/runner.js
@@ -0,0 +1,66 @@
+/**
+ * Run integration tests
+ *
+ * Uses the `filefog-provider-tests` module to
+ * run mocha tests against the appropriate version
+ * of Filefog.
+ */
+
+
+/**
+ * Module dependencies
+ */
+
+var mocha = require('mocha');
+var TestRunner = require('filefog-provider-tests');
+var Definition = require('../index.js');
+var winston = require('winston');
+
+
+// Grab targeted interfaces from this adapter's `package.json` file:
+var package = {}
+try {
+    package = require('../package.json');
+} catch (e) {
+    throw new Error(
+            '\n' +
+            'Could not read package.json :: ' + '\n' +
+            util.inspect(e)
+    );
+}
+
+
+
+winston.info('Testing `' + package.name + '`, a Filefog provider definition.');
+winston.info('Running `filefog-provider-tests`... ');
+winston.log();
+
+
+
+/**
+ * Integration Test Runner
+ *
+ * Uses the `waterline-adapter-tests` module to
+ * run mocha tests against the specified interfaces
+ * of the currently-implemented Waterline adapter API.
+ */
+new TestRunner({
+
+    // Mocha opts
+    mocha: {
+        bail: true
+    },
+
+    // Load the provider module.
+    definition: Definition,
+
+    // Default connection config to use.
+    config: {
+        client_key : 'cch3sssk23ueqsbdh2k2zlv2i7rz06lp',
+        client_secret : '6v7ywbCdut5FRdIjDeREofrFGc2ymGmA',
+        redirect_url : 'http://localhost:3000/service/callback/box'
+    },
+    name: package.name
+
+
+});
